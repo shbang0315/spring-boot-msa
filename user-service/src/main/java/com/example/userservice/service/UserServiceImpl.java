4eec269e754b6d8aadf47597684991e126a9dbe8
edowon0623@gmail.com <Dwlee$09>
2025-07-31 07:03:59 +0900

    added an exception

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -92,7 +93,13 @@ public class UserServiceImpl implements UserService {
 //                });
 //        List<ResponseOrder> orderList = orderListResponse.getBody();
 
-        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        List<ResponseOrder> orderList = null;
+        try {
+            orderList = orderServiceClient.getOrders(userId);
+        } catch (FeignException ex) {
+            log.error(ex.getMessage());
+        }
+
         userDto.setOrders(orderList);
 
         return userDto;
