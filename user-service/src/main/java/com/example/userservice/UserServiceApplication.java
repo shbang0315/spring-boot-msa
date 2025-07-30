c9b2d58d8f579e1de6be592349399c11bd971ae8
edowon0623@gmail.com <Dwlee$09>
2025-07-30 11:22:52 +0900

    added a loadnbalced

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -33,6 +34,7 @@ public class UserServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
